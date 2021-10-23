fd5b318e5019fc803194ca6c55652c086ba9cc2d
Maddieknz <maddelynneperrin@gmail.com>
2021-10-23 10:28:17 -0700

    Initial commit

---
+++ src/HydrationMod.cs
@@ -0,0 +1,29 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+[assembly: ModInfo( "Hydration Mod",
+	Description = "Adds hydration needs and items to Vintage Story",
+	Website     = "https://github.com/maddieknz/hydration",
+	Authors     = new []{ "maddieknz" } )]
+
+namespace MaddieKnz.Hydration
+{
+	public class HydrationMod : ModSystem
+	{
+		public override void Start(ICoreAPI api)
+		{
+			api.RegisterEntityBehaviorClass("hydration", typeof(HydrationBehavior))
+		}
+		
+		public override void StartClientSide(ICoreClientAPI api)
+		{
+			
+		}
+		
+		public override void StartServerSide(ICoreServerAPI api)
+		{
+			
+		}
+	}
+}
